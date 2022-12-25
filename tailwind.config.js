3d35d42b8a448040a3cbd10e6003ab522216a997
Muhammad Faris <muhfaris@disroot.org>
2022-12-25 21:38:53 +0700

    fix: add custom color to safelist tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{njk, md}",
-  ],
+  content: ["./src/**/*.{njk, md}"],
   mode: "jit",
   theme: {
     extend: {},
   },
   plugins: [],
+  safelist: ["bg-[#4A8DF9]", "bg-[#24D69E]", "bg-[#f06292]"],
 };
