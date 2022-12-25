9191c11725cddd47d3521c3c0cceb022c24063c4
Muhammad Faris <muhfaris@disroot.org>
2022-12-25 11:36:52 +0700

    fix: add njk in tailwindcss config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./_site/**/*.{html, js}"],
+  content: ["./_site/**/*.{html, js, njk}"],
   theme: {
     extend: {},
   },
