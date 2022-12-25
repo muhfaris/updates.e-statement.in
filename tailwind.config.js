976c2c99c1f856fa5d38c095b11f00d031cb59b3
Muhammad Faris <muhfaris@disroot.org>
2022-12-25 13:26:07 +0700

    fix: build tailwindcss to production

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./_site/**/*.{html, js}"],
+  content: [
+    "./src/**/*.{njk, md}",
+  ],
   mode: "jit",
-  // These paths are just examples, customize them to match your project structure
-  purge: ["./_site/**/*.{html, js}"],
   theme: {
     extend: {},
   },
